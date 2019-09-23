5d88025c2cd169815f0972a366ebaf40152dae2c
DecaDenZ <potapovdenis09@gmail.com>
2019-09-23 07:45:44 +0300

    2

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import './App.css';
 import Table from './table/table';
 
 function App(){
-  const [currentPlayer, setState] = useState(1);
-  const field = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-  ];
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [field, setField] = useState([
+    [1,2,0,0,0,0],
+    [2,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [2,1,1,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+  ])
 
   function move(columnId){
     alert('ход игрока ' + currentPlayer + '  в столбце  ' + columnId);
-    currentPlayer === 1 ? setState(2) : setState(1);
     columnId--;
     let position = field[columnId].indexOf(0);
-
-    })
+    field[columnId][position] = currentPlayer;
+    setField(field);
+    currentPlayer === 1 ? setCurrentPlayer(2) : setCurrentPlayer(1);
   }
-
 
     return (
       <div className="App">
